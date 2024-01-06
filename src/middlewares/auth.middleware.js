9f76763ff21faa9f14886c9866a37cf7ab4ff995
Impluse08 <pranavsarwe007@gmail.com>
2024-01-06 19:35:47 +0530

    minor change

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,7 +8,6 @@ import jwt from "jsonwebtoken"
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
             const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
-            
             if (!token){
                 throw new ApiError(400, "Unauthorized request")
             }
